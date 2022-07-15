49de1cbb0ac04e787aee3774fb286feda4177a70
CocoDiet <chiblossom00@gmail.com>
2022-07-15 16:43:15 +0000

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+A module for MyList class
+"""
+
+
+class MyList(list):
+    """A class that inherits from list."""
+    def print_sorted(self):
+        """A method that prints the list, but sorted """
+        print(sorted(self))
