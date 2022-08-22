14412ebd497795421c79c825763537f0de7f9802
CocoDiet <chiblossom00@gmail.com>
2022-08-22 20:50:22 +0000

    sql-query

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
-- script that creates the database and table cities on mySQL
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Script that creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; -- Query to create database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-state_id INT NOT NULL,
-FOREIGN KEY(state_id) REFRENCES states(id),
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( -- Query to create cities in hbtn_0d_usa
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id));
